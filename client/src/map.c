ba321cf50ac89ac1e2de10afc33a320a1784d237
Nick Qu <nick-quying@github.com>
2021-12-28 11:08:25 +0800

    modify bug
    Signed-off-by: Nick Qu <nick-quying@github.com>

--- client/src/map.c
+++ client/src/map.c
@@ -5,7 +5,19 @@ void init_map()
     for (int i = 0; i < map_width; i++) {
         test_map[0][i] = 2;
     }
-    for (int i = 0; i < map_length; i++) {
+    for (int i = 0; i < map_width; i++) {
+        test_map[map_length - 1][i] = 2;
+    }
+    for (int i = 1; i < map_length; i++) {
         test_map[i][0] = 3;
+    }
+    for (int i = 1; i < map_length; i++) {
+        test_map[i][map_width - 1] = 3;
+    }
+    for (int i = 10; i < map_length; i++) {
+        test_map[i][map_width / 2] = 3;
+    }
+     for (int i = 1; i < 10; i++) {
+        test_map[i][map_width / 4] = 3;
     }
 }
